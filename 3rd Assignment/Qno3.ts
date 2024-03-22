ba62ea9b2e5f7a69bd199a06052dafd2ee779a8d
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2024-03-21 23:22:39 -0700

    Add files via upload

---
+++ 3rd Assignment/Qno3.ts
@@ -0,0 +1,5 @@
+//Famous Quote: Find a quote from famous person you admire. Print quote and the name its author
+
+//William Shakespeare onces said, "No legacy is so rich as honesty"
+
+console.log('William Shakespeare onces said, "No legacy is so rich as honesty"')
