8a8b5378c3dbe96e5f90a49a42d47d51816a8f56
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2024-03-21 23:18:15 -0700

    Add files via upload

---
+++ 1st Assignment/question1.js
@@ -0,0 +1,4 @@
+//store a person name in variable and print message to that person
+var personname = 'jack';
+var message = " hello ".concat(personname, " woruld you like to play with me ");
+console.log(message);
---
+++ 1st Assignment/question1.ts
@@ -0,0 +1,6 @@
+//store a person name in variable and print message to that person
+
+let personname: string ='jack';
+let message: string = ` hello ${personname} woruld you like to play with me `
+console.log(message);
+
