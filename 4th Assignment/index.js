56098141c94f89d0616186bdd8fae431c1a1fdd3
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2024-04-30 01:41:40 -0700

    Add files via upload

---
+++ 4th Assignment/index.js
@@ -0,0 +1,4 @@
+//famous Quote 2: Repeat Exercise 4 but this time store person name in a variable called famous person then compose your message and store it in a new variable  called message print your message
+var famousperson = "William Shakepeare";
+var message = "".concat(famousperson, "  onces said, \"No legacy is so rich as honesty\" ");
+console.log(message);
---
+++ 4th Assignment/index.ts
@@ -0,0 +1,7 @@
+//famous Quote 2: Repeat Exercise 4 but this time store person name in a variable called famous person then compose your message and store it in a new variable  called message print your message
+
+let famousperson: string = "William Shakepeare"
+
+let message: string = `${famousperson}  onces said, "No legacy is so rich as honesty" `
+
+console.log(message);
